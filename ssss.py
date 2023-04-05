324998f941926018c12130c2fb73dd328bc52723
Alan A. Perez <alan80perez@gmail.com>
2023-04-05 12:29:10 -0400

    Funcional (aunque desmodalurizada) version del programa

--- ssss.py
+++ ssss.py
@@ -5,41 +5,21 @@ import openpyxl as op
 import ReadExcel as rxls
 
 
-m = rxls.reader("values.xlsx")
+names = rxls.reader("values.xlsx")
 print()
 
-fil = op.load_workbook("values.xlsx")
-sheet = fil.get_active_sheet()
-for cellObj in sheet.columns[0]:
-    print(cellObj.value)
-
-
-values = pandas.read_excel('values.xlsx', index_col=0)
-
-print(values['Juan'])
-des = 1 
-while True:
-    
-    print("Deposite los datos solicitados")
-    print()
-
-    name = input("Nombre: ")
-    present_name = input("Nombre Presentador: ")
-    date = input("Fecha: ")
+for i in range(len(names)):
+    name = names[i]
+    present_name = "Alan Perez"
+    date = "12/2/2023"
     
     doc = DocxTemplate("value.docx")
     context = { 'Name' : name, 'Presentator' : present_name, 'Date' : date }
     doc.render(context)
-    doc.save(f"{name}.docx")
-    convert(f"{name}.docx")
-    
-    os.system(f"\"{name}.pdf\"")
+    doc.save(f"{name}_{i}.docx")
+    convert(f"{name}_{i}.docx")
+    os.system(f"del {name}_{i}.docx")
 
-    des = int(input("""¿Desea proseguir?
-    1) Si
-    2) No
-    """))
+    os.system(f"{name}_{i}.pdf")
 
-    if des == 2:
-        context = {}
-        break
+  
